c13df5823289c3efb1405359025009577ae6a5e5
Alexey Karnachev <alekseykarnachev@gmail.com>
2024-02-08 15:30:43 +0400

    implement frustums cascade drawing

--- examples/rayfrustum.c
+++ examples/rayfrustum.c
@@ -5,6 +5,7 @@
 #include "rcamera.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define RAYGIZMO_IMPLEMENTATION
 #include "raygizmo.h"
@@ -18,7 +19,28 @@ typedef struct Frustum {
     // near_left_bot, near_left_top, near_right_top, near_right_bot
     // far_left_bot, far_left_top, far_right_top, far_right_bot
     Vector3 corners[8];
+
+    Matrix view;
 } Frustum;
+
+#define MAX_N_FRUSTUMS_IN_CASCADE 9
+typedef struct FrustumsCascade {
+    int n_frustums;
+    Frustum frustums[MAX_N_FRUSTUMS_IN_CASCADE];
+    float planes[MAX_N_FRUSTUMS_IN_CASCADE + 1];
+} FrustumsCascade;
+
+static Color FRUSTUM_COLORS[MAX_N_FRUSTUMS_IN_CASCADE] = {
+    (Color){255, 0, 0, 80},     // Red
+    (Color){0, 255, 0, 80},     // Green
+    (Color){0, 0, 255, 80},     // Blue
+    (Color){255, 255, 0, 80},   // Yellow
+    (Color){255, 0, 255, 80},   // Magenta
+    (Color){0, 255, 255, 80},   // Cyan
+    (Color){255, 128, 0, 80},   // Orange
+    (Color){128, 0, 128, 80},   // Purple
+    (Color){0, 128, 128, 80}    // Teal
+};
 
 typedef struct Triangle {
     Vector3 v1;
@@ -33,10 +55,16 @@ static Camera3D CAMERA_1;
 static Model CAMERA_MODEL;
 
 static Frustum get_frustum(Camera3D camera, float aspect, float near, float far);
-static Vector3 get_triangle_center(Triangle triangle);
+static FrustumsCascade get_frustums_cascade(
+    Camera3D camera,
+    float aspect,
+    float planes[MAX_N_FRUSTUMS_IN_CASCADE + 1],
+    int n_planes
+);
 static void update_free_orbit_camera(Camera3D *camera);
 static void draw_camera(Camera3D camera);
 static void draw_frustum(Frustum frustum, Color color);
+static void draw_frustums_cascade(FrustumsCascade cascade, Vector3 eye);
 
 int main(void) {
     SetConfigFlags(FLAG_MSAA_4X_HINT);
@@ -61,9 +89,8 @@ int main(void) {
         update_free_orbit_camera(&CAMERA_0);
 
         float aspect = (float)GetScreenWidth() / GetScreenHeight();
-        Frustum frustum_0 = get_frustum(CAMERA_1, aspect, 0.1, 2.0);
-        Frustum frustum_1 = get_frustum(CAMERA_1, aspect, 2.0, 6.0);
-        Frustum frustum_2 = get_frustum(CAMERA_1, aspect, 6.0, 20.0);
+        float planes[6] = {0.01, 2.0, 4.0, 8.0, 16.0, 32.0};
+        FrustumsCascade cascade = get_frustums_cascade(CAMERA_1, aspect, planes, 6);
 
         BeginDrawing();
         ClearBackground(CLEAR_COLOR);
@@ -71,20 +98,7 @@ int main(void) {
 
         draw_camera(CAMERA_1);
         DrawCube((Vector3){-1.0, 0.0, 0.0}, 1.0, 1.0, 1.0, PURPLE);
-
-        Vector3 d0 = Vector3Normalize(Vector3Subtract(CAMERA_0.target, CAMERA_0.position));
-        Vector3 d1 = Vector3Normalize(Vector3Subtract(CAMERA_1.target, CAMERA_1.position));
-        float f = Vector3DotProduct(d0, d1);
-        printf("%f\n", f);
-        if (f < 0.0) {
-            draw_frustum(frustum_0, ColorAlpha(RED, 0.5));
-            draw_frustum(frustum_1, ColorAlpha(GREEN, 0.5));
-            draw_frustum(frustum_2, ColorAlpha(BLUE, 0.5));
-        } else {
-            draw_frustum(frustum_2, ColorAlpha(BLUE, 0.5));
-            draw_frustum(frustum_1, ColorAlpha(GREEN, 0.5));
-            draw_frustum(frustum_0, ColorAlpha(RED, 0.5));
-        }
+        draw_frustums_cascade(cascade, CAMERA_0.position);
             
         DrawGrid(20, 1.0);
 
@@ -100,39 +114,52 @@ static Frustum get_frustum(Camera3D camera, float aspect, float near, float far)
     Matrix p_inv = MatrixInvert(p);
 
     Frustum frustum = {
-        Vector3Unproject((Vector3){-1.0, -1.0, -1.0}, p, v),
-        Vector3Unproject((Vector3){-1.0, 1.0, -1.0}, p, v),
-        Vector3Unproject((Vector3){1.0, 1.0, -1.0}, p, v),
-        Vector3Unproject((Vector3){1.0, -1.0, -1.0}, p, v),
-        Vector3Unproject((Vector3){-1.0, -1.0, 1.0}, p, v),
-        Vector3Unproject((Vector3){-1.0, 1.0, 1.0}, p, v),
-        Vector3Unproject((Vector3){1.0, 1.0, 1.0}, p, v),
-        Vector3Unproject((Vector3){1.0, -1.0, 1.0}, p, v)
+        .corners = {
+            Vector3Unproject((Vector3){-1.0, -1.0, -1.0}, p, v),
+            Vector3Unproject((Vector3){-1.0, 1.0, -1.0}, p, v),
+            Vector3Unproject((Vector3){1.0, 1.0, -1.0}, p, v),
+            Vector3Unproject((Vector3){1.0, -1.0, -1.0}, p, v),
+            Vector3Unproject((Vector3){-1.0, -1.0, 1.0}, p, v),
+            Vector3Unproject((Vector3){-1.0, 1.0, 1.0}, p, v),
+            Vector3Unproject((Vector3){1.0, 1.0, 1.0}, p, v),
+            Vector3Unproject((Vector3){1.0, -1.0, 1.0}, p, v)
+        },
+        .view = v
     };
 
     return frustum;
 }
 
-Vector3 get_triangle_center(Triangle triangle) {
-    Vector3 center;
+static FrustumsCascade get_frustums_cascade(
+    Camera3D camera,
+    float aspect,
+    float planes[MAX_N_FRUSTUMS_IN_CASCADE + 1],
+    int n_planes
+) {
+    if (n_planes < 2 || n_planes > MAX_N_FRUSTUMS_IN_CASCADE + 1) {
+        fprintf(
+            stderr,
+            "ERROR: Number of frustum planes must be >= 2 and <= %d, but you passed %d\n",
+            MAX_N_FRUSTUMS_IN_CASCADE + 1, n_planes
+        );
+        exit(1);
+    }
     
-    center.x = (triangle.v1.x + triangle.v2.x + triangle.v3.x) / 3.0f;
-    center.y = (triangle.v1.y + triangle.v2.y + triangle.v3.y) / 3.0f;
-    center.z = (triangle.v1.z + triangle.v2.z + triangle.v3.z) / 3.0f;
-    
-    return center;
-}
+    FrustumsCascade cascade = {0};
+    memcpy(cascade.planes, planes, sizeof(planes[0]) * n_planes);
 
-static int compare_triangles(const void *triangle_0, const void *triangle_1) {
-    Triangle *t0 = (Triangle*)triangle_0;
-    Triangle *t1 = (Triangle*)triangle_1;
+    for (int i = 0; i < n_planes - 1; ++i) {
+        float near = planes[i];
+        float far = planes[i + 1];
+        if (far <= near) {
+            fprintf(stderr, "ERROR: Frustum planes must be in ascending order\n");
+            exit(1);
+        }
 
-    float da = Vector3Length(get_triangle_center(*t0));
-    float db = Vector3Length(get_triangle_center(*t1));
+        cascade.frustums[cascade.n_frustums++] = get_frustum(camera, aspect, near, far);
+    }
 
-    if (da < db) return 1;
-    else if (da > db) return -1;
-    else return 0;
+    return cascade;
 }
 
 static void update_free_orbit_camera(Camera3D *camera) {
@@ -203,3 +230,39 @@ static void draw_frustum(Frustum frustum, Color color) {
     }
 }
 
+static void draw_frustums_cascade(FrustumsCascade cascade, Vector3 eye) {
+    // -------------------------------------------------------------------
+    // Get the distance of the eye on the z (view) axis of the cascade
+    Matrix view = cascade.frustums[0].view;
+    float z = -Vector3Transform(eye, view).z;
+
+    // -------------------------------------------------------------------
+    // Find nearest frustum (this will be drawn last)
+    int nearest_frustum_idx = 0;
+    if (z <= cascade.planes[0]) {
+        nearest_frustum_idx = 0;
+    } else if (z >= cascade.planes[cascade.n_frustums]) {
+        nearest_frustum_idx = cascade.n_frustums - 1;
+    } else {
+        for (int i = 0; i < cascade.n_frustums; ++i) {
+            float near = cascade.planes[i];
+            float far = cascade.planes[i + 1];
+            if (z >= near && z <= far) {
+                nearest_frustum_idx = i;
+                break;
+            }
+        }
+    }
+
+    // -------------------------------------------------------------------
+    // Draw frustums in the order furthest -> nearest
+    for (int i = 0; i < nearest_frustum_idx; i++) {
+        draw_frustum(cascade.frustums[i], FRUSTUM_COLORS[i]);
+    }
+
+    for (int i = cascade.n_frustums - 1; i > nearest_frustum_idx; i--) {
+        draw_frustum(cascade.frustums[i], FRUSTUM_COLORS[i]);
+    }
+
+    draw_frustum(cascade.frustums[nearest_frustum_idx], FRUSTUM_COLORS[nearest_frustum_idx]);
+}
